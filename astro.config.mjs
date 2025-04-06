5a3d7b36adb2769a6d9e0eca20fff4a51541eb83
Indroic <indroic@outlook.es>
2025-04-06 01:40:10 -0400

    ya casi

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://indroic.vercel.app', // Cambia esto a la URL de tu sitio
   vite: {
     plugins: [tailwindcss()],
   },
