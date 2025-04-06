fd357c078d9dbf2520dbfe0a34769c848eb3bc7b
Indroic <indroic@outlook.es>
2025-04-06 01:51:01 -0400

    eliminado del archivo de config

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,7 @@ import { defineMiddleware } from "astro:middleware";
 import translate from "@translate";
 
 export const onRequest = defineMiddleware((context, next) => {
-    const defaultLang = Object.keys(translate)[0]; // Idioma predeterminado (por ejemplo, "en")
-    console.log(context.url.pathname.split("/"))
-    console.log(context.url.pathname.split("/").length)
+    const defaultLang = Object.keys(translate)[0];
     if (context.url.pathname.split("/")[1] === '') {
         return context.redirect(`/${defaultLang}`);
     }
