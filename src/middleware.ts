95ab351dbda96acb61077b5a46e880dc22f4908f
Indroic <indroic@outlook.es>
2025-04-06 01:07:31 -0400

    correcciones

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,9 @@
 import { defineMiddleware } from "astro:middleware";
-import translate from "@translate";
+import translate from "@translate"
 
 export const onRequest = defineMiddleware((context, next) => {
-    const defaultLang = Object.keys(translate)[0]; // Idioma predeterminado (por ejemplo, "en")
-    const path = context.url.pathname;
-
-    // Verificar si la ruta no tiene un prefijo de idioma
-    if (!path.match(/^\/[a-z]{2}(\/|$)/)) {
-        // Redirigir agregando el idioma predeterminado al final de la ruta
-        return context.redirect(`${path.replace(/\/$/, "")}/${defaultLang}`);
+    if (context.url.pathname === "/") {
+        return context.redirect(`/${Object.keys(translate)[0]}`);
     }
 
     return next();
